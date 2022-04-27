c359f681ef707cf85c91540cb5d22fa4db74f70f
chamomile <55081468+cat-boop@users.noreply.github.com>
2022-04-27 15:09:57 +0300

    made classes non-static

---
+++ lab-common/src/main/java/com/lab7/common/util/Deserializer.java
@@ -0,0 +1,45 @@
+package com.lab7.common.util;
+
+import com.lab7.exceptions.DeserializeException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+public class Deserializer {
+    private final byte[] serializedObject;
+    private Serializable deserializedObject;
+    private boolean alreadyDeserialized;
+
+    public Deserializer(byte[] serializedObject) {
+        this.serializedObject = serializedObject;
+        alreadyDeserialized = false;
+    }
+
+    public Serializable deserialize() {
+        if (alreadyDeserialized) {
+            return deserializedObject;
+        }
+        try (ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(serializedObject);
+             ObjectInputStream objectInputStream = new ObjectInputStream(arrayInputStream)) {
+                deserializedObject = (Serializable) objectInputStream.readObject();
+                alreadyDeserialized = true;
+                return deserializedObject;
+        } catch (IOException | ClassNotFoundException e) {
+            throw new DeserializeException("Невозможно десериализовать объект");
+        }
+    }
+
+    public boolean possibleToDeserialize() {
+        if (alreadyDeserialized) {
+            return true;
+        }
+        try {
+            deserialize();
+            return true;
+        } catch (DeserializeException e) {
+            return false;
+        }
+    }
+}
---
+++ lab-common/src/main/java/com/lab7/common/util/Serializer.java
@@ -0,0 +1,46 @@
+package com.lab7.common.util;
+
+import com.lab7.exceptions.SerializeException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public final class Serializer {
+    private byte[] serializedObject;
+    private final Serializable objectToSerialize;
+    private boolean alreadySerialized;
+
+    public Serializer(Serializable objectToSerialize) {
+        this.objectToSerialize = objectToSerialize;
+    }
+
+    public byte[] serialize() {
+        if (alreadySerialized) {
+            return serializedObject;
+        }
+        try (ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+             ObjectOutputStream outputStream = new ObjectOutputStream(arrayOutputStream)) {
+            outputStream.writeObject(objectToSerialize);
+            outputStream.flush();
+            serializedObject = arrayOutputStream.toByteArray();
+            alreadySerialized = true;
+            return serializedObject;
+        } catch (IOException e) {
+            throw new SerializeException("Невозможно сериализовать запрос");
+        }
+    }
+
+    public boolean possibleToSerialize() {
+        if (alreadySerialized) {
+            return true;
+        }
+        try {
+            serialize();
+            return true;
+        } catch (SerializeException e) {
+            return false;
+        }
+    }
+}
