16b6d8517f7efe15208238944d27873eb8da8947
chamomile <55081468+cat-boop@users.noreply.github.com>
2022-04-27 15:15:41 +0300

    now server stop work without system exit

---
+++ lab-server/src/main/java/com/lab7/server/ServerConsoleListener.java
@@ -0,0 +1,38 @@
+package com.lab7.server;
+
+import com.lab7.server.logger.ServerLogger;
+
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ServerConsoleListener extends Thread {
+    private static boolean serverShouldWork;
+    private final Scanner scanner;
+
+    public ServerConsoleListener(Scanner scanner) {
+        this.scanner = scanner;
+        serverShouldWork = true;
+    }
+
+    @Override
+    public void run() {
+        while (serverShouldWork) {
+            try {
+                String commandName = scanner.nextLine().trim().toLowerCase(Locale.ROOT);
+                ServerLogger.logInfoMessage("На сервера введена команда \"{}\"", commandName);
+                if ("exit".equals(commandName)) {
+                    serverShouldWork = false;
+                    ServerLogger.logInfoMessage("Работа сервера остановлена");
+                }
+            } catch (NoSuchElementException e) {
+                serverShouldWork = false;
+                ServerLogger.logInfoMessage("Работа сервера остановлена");
+            }
+        }
+    }
+
+    public static boolean isServerShouldWork() {
+        return serverShouldWork;
+    }
+}
