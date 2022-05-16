d300d797f9e873ce0d6d04d38a47441cb24c0e88
chamomile <55081468+cat-boop@users.noreply.github.com>
2022-05-16 13:59:00 +0300

    add supplier that create new command with every new request

--- lab-server/src/main/java/com/lab7/server/CommandManager.java
+++ lab-server/src/main/java/com/lab7/server/CommandManager.java
@@ -21,40 +21,41 @@ import com.lab7.server.database.DBManager;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * Class that execute user commands
  */
 public class CommandManager {
-    private static Map<String, AbstractCommand> commands;
+    private final Map<String, Supplier<AbstractCommand>> commands;
     private final DBManager dbManager;
 
     public CommandManager(DBManager dbManager, CollectionManager collectionManager) {
         this.dbManager = dbManager;
         commands = Collections.synchronizedMap(new HashMap<>());
-        commands.put("add", new AddCommand(dbManager, collectionManager));
-        commands.put("add_if_min", new AddIfMinCommand(dbManager, collectionManager));
-        commands.put("clear", new ClearCommand(dbManager, collectionManager));
-        commands.put("count_greater_than_distance", new CountGreaterThanDistanceCommand(collectionManager));
-        commands.put("count_less_than_distance", new CountLessThanDistanceCommand(collectionManager));
-        commands.put("help", new HelpCommand());
-        commands.put("info", new InfoCommand(collectionManager));
-        commands.put("max_by_distance", new MaxByDistanceCommand(collectionManager));
-        commands.put("remove_by_id", new RemoveByIdCommand(dbManager, collectionManager));
-        commands.put("remove_greater", new RemoveGreaterCommand(dbManager, collectionManager));
-        commands.put("remove_lower", new RemoveLowerCommand(dbManager, collectionManager));
-        commands.put("show", new ShowCommand(collectionManager));
-        commands.put("update", new UpdateCommand(dbManager, collectionManager));
-        commands.put("connect_user", new ConnectUserCommand(dbManager));
-        commands.put("register_user", new RegisterUserCommand(dbManager));
+        commands.put("add", () -> new AddCommand(dbManager, collectionManager));
+        commands.put("add_if_min", () -> new AddIfMinCommand(dbManager, collectionManager));
+        commands.put("clear", () -> new ClearCommand(dbManager, collectionManager));
+        commands.put("count_greater_than_distance", () -> new CountGreaterThanDistanceCommand(collectionManager));
+        commands.put("count_less_than_distance", () -> new CountLessThanDistanceCommand(collectionManager));
+        commands.put("info", () -> new InfoCommand(collectionManager));
+        commands.put("max_by_distance", () -> new MaxByDistanceCommand(collectionManager));
+        commands.put("remove_by_id", () -> new RemoveByIdCommand(dbManager, collectionManager));
+        commands.put("remove_greater", () -> new RemoveGreaterCommand(dbManager, collectionManager));
+        commands.put("remove_lower", () -> new RemoveLowerCommand(dbManager, collectionManager));
+        commands.put("show", () -> new ShowCommand(collectionManager));
+        commands.put("update", () -> new UpdateCommand(dbManager, collectionManager));
+        commands.put("connect_user", () -> new ConnectUserCommand(dbManager));
+        commands.put("register_user", () -> new RegisterUserCommand(dbManager));
+        commands.put("help", () -> new HelpCommand(this));
     }
 
-    public static Map<String, AbstractCommand> getCommands() {
+    public Map<String, Supplier<AbstractCommand>> getCommands() {
         return commands;
     }
 
     public AbstractCommand getCommandByName(String name) {
-        return commands.get(name);
+        return commands.get(name).get();
     }
 
     public DBManager getDbManager() {
